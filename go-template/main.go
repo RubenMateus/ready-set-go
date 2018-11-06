14dfc21c3c0c53496ef9412b24a5daa93a9509b6
Ruben Mateus <ruben.mateus3@gmail.com>
2018-11-06 01:20:34 +0000

    added mocked, example for query and render stuff, wip

--- go-template/main.go
+++ go-template/main.go
@@ -2,12 +2,41 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type SearchPage struct {
+	Query string
+	Books []Book
+}
+
+type Book struct {
+	Title  string
+	Author string
+	Year   int
+}
+
 func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, GO FOR TEMPLATING XD!\n")
+		if e := templates.ExecuteTemplate(w, "index.html", nil); e != nil {
+			http.Error(w, e.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		books := []Book{
+			Book{"Clean Code", "Robert C. Martin", 2008},
+			Book{"The Clean Coder", "Robert C. Martin", 2011},
+			Book{"An Introduction to programming in GO", "Caleb Doxsey", 2012},
+		}
+		p := SearchPage{Query: r.FormValue("search"), Books: books}
+
+		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 
 	fmt.Println(http.ListenAndServe(":504", nil))
