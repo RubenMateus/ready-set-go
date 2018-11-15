798bc6e782138ec77a51c631c7213766788117c8
Ruben Mateus <ruben.mateus3@gmail.com>
2018-11-15 00:54:00 +0000

    beautified html :D and changes to the main, using api for books

--- go-template/main.go
+++ go-template/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 type SearchPage struct {
@@ -12,32 +15,61 @@ type SearchPage struct {
 }
 
 type Book struct {
-	Title  string
-	Author string
-	Year   int
+	Title  string `xml:"title,attr"`
+	Author string `xml:"author,attr"`
+	Year   int    `xml:"hyr,attr"`
+	ID     string `xml:"owi,attr"`
 }
 
 func main() {
 	templates := template.Must(template.ParseFiles("templates/index.html"))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if e := templates.ExecuteTemplate(w, "index.html", nil); e != nil {
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		results, e := search(r.FormValue("search"))
+		if e != nil {
 			http.Error(w, e.Error(), http.StatusInternalServerError)
 		}
-	})
 
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		books := []Book{
-			Book{"Clean Code", "Robert C. Martin", 2008},
-			Book{"The Clean Coder", "Robert C. Martin", 2011},
-			Book{"An Introduction to programming in GO", "Caleb Doxsey", 2012},
-		}
-		p := SearchPage{Query: r.FormValue("search"), Books: books}
-
+		p := SearchPage{Query: r.FormValue("search"), Books: results}
 		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
 
-	fmt.Println(http.ListenAndServe(":504", nil))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println(http.ListenAndServe(":4000", nil))
+}
+
+func search(query string) ([]Book, error) {
+	var response struct {
+		Books []Book `xml:"works>work"`
+	}
+
+	body, err := fetch("title=" + url.QueryEscape(query))
+
+	if err != nil {
+		return []Book{}, err
+	}
+
+	err = xml.Unmarshal(body, &response)
+	return response.Books, err
+}
+
+func fetch(q string) ([]byte, error) {
+	var resp *http.Response
+	var err error
+	url := "http://classify.oclc.org/classify2/Classify?summary=true&" + q
+
+	if resp, err = http.Get(url); err != nil {
+		return []byte{}, err
+	}
+
+	defer resp.Body.Close()
+
+	return ioutil.ReadAll(resp.Body)
 }
